c0213fbc328c46a432d0fc7e170292aa9f5fe9c5
acarrascoh <angelo.cherrera1@gmail.com>
2024-08-26 18:12:30 -0400

    agregar, eliminar y mostrar basicos

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -19,20 +19,53 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 })
 export class AppComponent {
   title = 'todo';
-
-  usuarios: any=[];
-
   APIURL = 'http://localhost:5240/';
+  usuarios: any=[];
+  
+  nuevoUsuarioForm!: FormGroup;
 
-  constructor(private http: HttpClient) {}
+
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {}
 
   ngOnInit() {
-    this.get_tasks();
+    this.get_usuarios();
+    this.nuevoUsuarioForm= this.formBuilder.group({
+      Rut:['',[Validators.required]],  
+      Nombre:['',[Validators.required]],
+      Edad:['',[Validators.required]]
+    })
   }
 
-  get_tasks(){
+  get_usuarios(){
     this.http.get(this.APIURL+"api/usuario").subscribe((res)=>{
       this.usuarios=res;
     })
+  }
+
+  agregarUsuario(){
+    const data={
+      Rut: this.nuevoUsuarioForm.value.Rut,
+      Nombre: this.nuevoUsuarioForm.value.Nombre,
+      Edad: this.nuevoUsuarioForm.value.Edad,
+    }
+    this.http.post(this.APIURL+"api/usuario",data).subscribe({
+      next: (res:any) => {
+          alert("Creación de usuario exitoso");
+          this.get_usuarios();
+          this.nuevoUsuarioForm.reset();
+      },
+      error: (e) => {
+        console.error(e);
+      }
+    });
+
+  }
+
+  eliminarUsuario(usuarioPK:any){
+    this.http.delete(this.APIURL+"api/usuario/"+usuarioPK).subscribe((res)=>{
+      this.get_usuarios();
+    
+    });
+
   }
 }
